cea2c48d2e0c0976103d5a5d18e04412c3e910bb
sreedSorenson <sreed@sorenson.com>
2020-11-30 14:25:40 -0700

    Inital staging

---
+++ Deaf_Academy/Deaf_Academy/ClientApp/src/components/Courses.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+export class Courses extends Component {
+    static displayName = Courses.name;
+
+    render() {
+        return (
+            <div>
+                <h1>Course</h1>
+                <p>Course page</p>
+            </div>
+        );
+    }
+}
